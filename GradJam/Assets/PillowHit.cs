c35a8bf432e859c278e917aba517301800c8f389
sasstron <sarah.assmus@gmail.com>
2021-05-22 18:27:34 -0400

    Left Right Trigger working

--- GradJam/Assets/PillowHit.cs
+++ GradJam/Assets/PillowHit.cs
@@ -5,30 +5,50 @@ using UnityEngine;
 public class PillowHit : MonoBehaviour
 {
     private Animator pillowAnim;
-    public bool swing = false;
+    public bool leftHit = true;
 
     void Start()
     {
-        pillowAnim = gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>();
+        pillowAnim = gameObject.GetComponent<Animator>();
     }
+    public void EndSwingLeft()
+    {
+        pillowAnim.SetBool("swingLeftBool", false);
+        //print("left done");
 
+    }
+    public void EndSwingRight()
+    {
 
+        pillowAnim.SetBool("swingRightBool", false);
+        //print("right done");
+
+    }
     void Update()
     {
+        
+        
         if (Input.GetMouseButtonDown(0))
         {
-            if (!swing) //if you swing for the first time
+            if (leftHit)
             {
+                //pillowAnim.SetBool("swingLeftBool", false);
                 pillowAnim.SetBool("swingLeftBool", true);
-                
+                //print("time to swing right");
+                leftHit = false;
+
             }
-            else if (swing)
+            else
             {
-                pillowAnim.SetBool("swingBool", false);
+                //pillowAnim.SetBool("swingRightBool", false);
+                pillowAnim.SetBool("swingRightBool", true);
+                //print("time to swing left");
+                leftHit = true;
 
             }
+            
+            //pillowAnim.SetBool("startBlendTree", true);
 
-            swing = true;
 
         }
     }
