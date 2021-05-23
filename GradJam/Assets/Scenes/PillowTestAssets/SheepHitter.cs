dd8459d37eecfcd629e0f875da502fc79791bc83
sasstron <sarah.assmus@gmail.com>
2021-05-23 15:12:44 -0400

    Push push push prefab changes sounds n jazz

--- GradJam/Assets/Scenes/PillowTestAssets/SheepHitter.cs
+++ GradJam/Assets/Scenes/PillowTestAssets/SheepHitter.cs
@@ -9,8 +9,8 @@ public class SheepHitter : MonoBehaviour
     //The sheeps' current health point total
     public int currentHealth;
     public GameObject poofPrefab;
-    //private AudioSource sheepSound;
-    //public List<AudioClip> mySheepSounds = new List<AudioClip>();
+    private AudioSource sheepSound;
+    public List<AudioClip> mySheepSounds = new List<AudioClip>();
     public bool bigSheep;
     public bool smolSheep;
 
@@ -22,21 +22,19 @@ public class SheepHitter : MonoBehaviour
         sheepLocation = gameObject.transform.position;
         if (bigSheep) currentHealth = 3;
         if (smolSheep) currentHealth = 1;
-        //sheepSound = GetComponent<AudioSource>();
-        //AudioSource[] sounds;
-        //sounds = GetComponents<AudioSource>();
-        //sheepSound = sounds[0];
+        sheepSound = GetComponent<AudioSource>();
+
 
     }
     public void Damage(int damageAmount)
     {
         //subtract damage amount when Damage function is called
         currentHealth -= damageAmount;
-        //sheepSound.PlayOneShot(mySheepSounds[Random.Range(0, 3)]);
+        sheepSound.PlayOneShot(mySheepSounds[Random.Range(0, 2)]);
         //Check if health has fallen below zero
         if (currentHealth <= 0)
         {
-            //sheepSound.PlayOneShot(mySheepSounds[Random.Range(0, 3)]);
+            sheepSound.PlayOneShot(mySheepSounds[Random.Range(0, 2)]);
             //if health has fallen below zero, make it disappear 
             gameObject.SetActive(false);
             GameObject clone;
@@ -46,7 +44,7 @@ public class SheepHitter : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name);
+        //Debug.Log(other.name);
         if (other.gameObject.CompareTag("Pillow"))
         {
             Damage(1);
