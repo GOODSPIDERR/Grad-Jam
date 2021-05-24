1e7b308781139521df72053bdd5b495b1302ea5d
Vladimir Karashchuk <43484800+OffworldDemon@users.noreply.github.com>
2021-05-24 12:41:11 -0400

    Clock now glows & made platform hitboxes more forgiving

--- GradJam/Assets/Scripts/DummyScript.cs
+++ GradJam/Assets/Scripts/DummyScript.cs
@@ -10,13 +10,14 @@ public class DummyScript : MonoBehaviour
     private GameObject enemy;
     private FirstPersonController fpc;
     private Rigidbody erb;
-   // private AudioSource hitSound;
+    // private AudioSource hitSound;
     public float hearingRange, spottingRange, spottingAngle, pushForce;
     private bool agro, attack;
     Animator sheepAnim;
 
-    void Start(){
-        agro = attack =  false;
+    void Start()
+    {
+        agro = attack = false;
         agent = gameObject.GetComponent<NavMeshAgent>();
         enemy = GameObject.FindGameObjectsWithTag("Player")[0];
         fpc = enemy.GetComponent<FirstPersonController>();
@@ -32,38 +33,54 @@ public class DummyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(agro){
-            if(fpc.getCrouch() && hearing() > hearingRange && !spotting()){
+        if (agro)
+        {
+            if (fpc.getCrouch() && hearing() > hearingRange && !spotting())
+            {
                 agro = false;
                 sheepAnim.SetBool("agro", false);
                 agent.SetDestination(transform.position);
-            } else {
+            }
+            else
+            {
                 agent.SetDestination(enemy.transform.position);
-                if(hearing() <= 1){
+                if (hearing() <= 1)
+                {
                     attack = true;
                 }
             }
-        } else {
-            if(fpc.getSprint()){
-                if(hearing() < (2 * hearingRange)){
-                    agro = true;
-                    sheepAnim.SetBool("agro", true);
-                    agent.SetDestination(enemy.transform.position);
-                }
-            } else if(fpc.getCrouch()) {
-                if(hearing() < (0.5 * hearingRange)){
-                    agro = true;
-                    sheepAnim.SetBool("agro", true);
-                    agent.SetDestination(enemy.transform.position);
-                }
-            } else {
-                if(hearing() < hearingRange){
+        }
+        else
+        {
+            if (fpc.getSprint())
+            {
+                if (hearing() < (1.5 * hearingRange))
+                {
                     agro = true;
                     sheepAnim.SetBool("agro", true);
                     agent.SetDestination(enemy.transform.position);
                 }
             }
-            if(spotting() && hearing() < spottingRange){
+            else if (fpc.getCrouch())
+            {
+                if (hearing() < (0.5 * hearingRange))
+                {
+                    agro = true;
+                    sheepAnim.SetBool("agro", true);
+                    agent.SetDestination(enemy.transform.position);
+                }
+            }
+            else
+            {
+                if (hearing() < hearingRange)
+                {
+                    agro = true;
+                    sheepAnim.SetBool("agro", true);
+                    agent.SetDestination(enemy.transform.position);
+                }
+            }
+            if (spotting() && hearing() < spottingRange)
+            {
                 agro = true;
                 sheepAnim.SetBool("agro", true);
                 agent.SetDestination(enemy.transform.position);
@@ -71,14 +88,17 @@ public class DummyScript : MonoBehaviour
         }
     }
 
-    void FixedUpdate(){
-        if(attack){
+    void FixedUpdate()
+    {
+        if (attack)
+        {
             knockback();
             attack = false;
         }
     }
 
-    double hearing(){
+    double hearing()
+    {
         double x, z, dist;
         x = transform.position.x - enemy.transform.position.x;
         z = transform.position.z - enemy.transform.position.z;
@@ -88,7 +108,8 @@ public class DummyScript : MonoBehaviour
         return dist;
     }
 
-    bool spotting(){
+    bool spotting()
+    {
         Vector3 targetDir = enemy.transform.position - transform.position;
         float angle = Vector3.Angle(targetDir, transform.forward);
 
@@ -96,18 +117,23 @@ public class DummyScript : MonoBehaviour
         layerMask = ~layerMask;
 
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, targetDir, out hit, Mathf.Infinity, layerMask)){
+        if (Physics.Raycast(transform.position, targetDir, out hit, Mathf.Infinity, layerMask))
+        {
             return false;
         }
 
-        if(angle < spottingAngle){
+        if (angle < spottingAngle)
+        {
             return true;
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
 
-    void knockback(){
+    void knockback()
+    {
         Vector3 targetDir = enemy.transform.position - transform.position;
         targetDir.y = 0;
         erb.AddForce(targetDir.normalized * pushForce, ForceMode.Impulse);
